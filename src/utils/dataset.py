3777b9f7cdcc9a2d8078a8327b179c5722006806
adssib <adibakkari@gmail.com>
2025-11-16 18:15:59 -0500

    changes for README, dataset utils

---
+++ src/utils/dataset.py
@@ -0,0 +1,11 @@
+## 
+# 1 function that will get me a dataset with no preprocessing given a specific Name you know . 
+# 
+# 2 a preprocessing function for every specific dataset, since every dataset have some noise in it. i.e the wikidataset contain \n & ' ' and "====" which will cluter the training stuff
+# also the sentiment analysis preprocessing is diffrent from a set to the other
+# 
+# we may have the datasets preprocessed already to use, or we could have the dataset and then call the preprocess 
+# 
+# 3 we need a function called         cleaned_dataset = preprocessDataset(batch_size, x, dataset_name) or getDataSetBatch(same parameters
+
+# ds = load_dataset("Salesforce/wikitext", "wikitext-2-raw-v1", split="train") & SST-2 & IMDB training & SQuAD v1.1
